77e47939cad492962bc0f997903377b2bf564af4
nguyenthanhchung090302 <110025385+nguyenthanhchung090302@users.noreply.github.com>
2022-08-11 10:12:43 +0700

    chung dz

---
+++ bài tập array/array.js
@@ -0,0 +1,81 @@
+//b1:
+// let myColor = ["Red", "Green", "White", "Black"];
+// let a = myColor.join(" , ");
+// console.log(a);
+
+
+//b2:
+// let number = prompt('nhập dãy số').split('');
+// let b=[];
+// for(let i = 0; i < number.length; i++) {
+//     b.push(number[i]);
+// }
+// let a = b.join('-');
+// console.log(a);
+
+//b4:
+// let a = prompt('Mời bạn nhập một dãy số, Các số được phân tách bằng dấu phẩy').split(',');
+// let tong = 0;
+// for(let i = 0; i < a.length; i++) {
+//     tong += Number(a[i]);
+// }
+// console.log(`tổng các số là: ${tong}`);
+
+//b5
+// let a = prompt('Mời bạn nhập một dãy số, các số được phân tách bằng dấu phẩy').split(',');
+// let min = a[0];
+// for(i = 0; i < a.length; i++) {
+//     if (min>a[i]) {
+//         min=a[i]
+//     }
+// }
+// alert(`Số nhỏ nhất trong dãy số là: ${min}`)
+
+//b6
+// let number = Number(prompt('nhập số bạn muốn tìm trong mảng'));
+// let indexOf = -1;
+// for (let i = 0; i < arr.length; i++) {
+//     if (number === arr[i]) {
+//         indexOf = i;
+//     }
+// }
+// if(indexOf===-1){
+//     console.log(`${number} is  is not found in my array` );
+// }else {
+//     console.log(`${number} is found in my array ${indexOf}`);
+// }
+
+
+//b8
+// let ten = prompt('enter a sequence of names');
+// let a = ten.split(',');
+// let arr =[];
+// for (let i = 0; i < a.length; i++) {
+//     arr.push(`<${a[i]}>`);
+// }
+// console.log(`${a}=>  ${arr}`);
+
+
+//b9
+// let number = prompt('nhập chữ số');
+// let a = number.split(',');
+// let arr = [];
+// for (let i = 0; i < a.length; i++) {
+//     if (Number(a[i]) % 2 == 1) {
+//         arr.push(a[i]);
+//     }
+// }
+// console.log(`${a} => ${arr}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
